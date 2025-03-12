ff2ae7aebfd48e290fdf4c93964c2b2e24c58b03
Seb <121176289+sebDtSci@users.noreply.github.com>
2025-03-12 23:52:13 +0100

    clearest

--- src/brmsAPI/payload_construction.py
+++ src/brmsAPI/payload_construction.py
@@ -1,4 +1,4 @@
-def payload_construction(nom:str,prenom:str,age:int,adresse:str, maisonPrice:int, sinistre:str = "Incendie"):
+def payload_construction(nom:str, prenom:str, age:int, adresse:str, maisonPrice:int, sinistre:str = "Incendie"):
     
     payload = {
         "personne": {
